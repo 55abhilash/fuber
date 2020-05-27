f7b66d4d6a1661791df3bbd6f1bd2040841394f7
Abhilash KP <abhilashmhaisne@gmail.com>
2020-05-27 20:21:24 +0530

    Clojure server initial config

--- fuber/src/fuber/core.clj
+++ fuber/src/fuber/core.clj
@@ -1,7 +1,21 @@
 (ns fuber.core
+  (:require [org.httpkit.server :as server]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer :all]
+            [clojure.pprint :as pp]
+            [clojure.string :as str]
+            [clojure.data.json :as json])
   (:gen-class))
 
+(defroutes app-routes
+  (GET "/getTaxt" [] getTaxt)
+  (GET "/endRide" [] endRide)
+  (route/not-found "Error! Page Not Found"))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Clojure Server"
   [& args]
-  (println "Hello, World!"))
+  (let [port (Integer/parseInt "3000")]
+    (server/run-server #'app-routes {:port port})
+  (println (str "Server Running at localhost:" port))))
