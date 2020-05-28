4e3c1738479cd186e923cacad46bae7cfa288ebc
Abhilash KP <abhilashmhaisne@gmail.com>
2020-05-29 04:45:32 +0530

    Minor fix: Return only latest ride id

--- src/fuber/api/start.clj
+++ src/fuber/api/start.clj
@@ -8,6 +8,6 @@
     (try (jdbc/with-db-transaction [t-con dbspec/dbspec]
            (jdbc/insert! t-con :ride {:taxi_id taxi_id :start_latitude start_lat :start_longitude start_long})
            (jdbc/update! t-con :taxi {:isAssigned 1} ["taxi_id = ?" taxi_id])
-           (jdbc/query t-con ["select ride_id, taxi_id from ride where taxi_id = ?" taxi_id]))
+           (jdbc/query t-con ["select max(ride_id), taxi_id from ride where taxi_id = ?" taxi_id]))
 
-         (catch Exception e (.printStackTrace (.getNextException e)))) {:err "This Taxi is no longer available. Run /findTaxi again to find nearest available Taxi."}))
+         (catch Exception e ( println (Throwable->map e)))) {:err "This Taxi is no longer available. Run /findTaxi again to find nearest available Taxi."}))
